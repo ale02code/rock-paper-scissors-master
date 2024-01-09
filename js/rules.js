e6457d9d0517c79f9e7dc402e0a7c92833494971
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-01-09 09:47:15 -0600

    add rule section

---
+++ js/rules.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const body = document.body;
+const buttonToRules = document.querySelector('.footer__rules');
+
+const ruleContainer = document.createElement('section');
+ruleContainer.classList.add('rule-container');
+
+const ruleTitle = document.createElement('h2');
+ruleTitle.textContent = 'Rules';
+ruleTitle.classList.add('rule-title');
+
+const ruleImage = document.createElement('img');
+ruleImage.src = 'images/image-rules-bonus.svg';
+ruleImage.alt = "rules image";
+ruleImage.classList.add('rule-image');
+
+const ruleExit = document.createElement('img');
+ruleExit.src = 'images/icon-close.svg';
+ruleExit.alt = 'close';
+ruleExit.classList.add('rule-exit');
+
+ruleExit.addEventListener('click', () => {
+  ruleContainer.style.display = "none";
+})
+
+buttonToRules.addEventListener('click', () => {
+  ruleContainer.style.display = "flex";
+})
+
+body.appendChild(ruleContainer);
+ruleContainer.appendChild(ruleTitle);
+ruleContainer.appendChild(ruleImage);
+ruleContainer.appendChild(ruleExit);
